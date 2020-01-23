af59a9a13b69ca18b831b47621d29f5a9608cb52
Otmar Braunecker <otmarvonwien@hotmail.com>
2020-01-23 12:14:16 +0100

    initial commit

---
+++ math-service/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+
+// Create a logger instance
+const logger = new Logger('Main');
+
+// Create the microservice options object
+const microserviceOptions = {
+  transport: Transport.REDIS,
+  options: {
+    url: 'redis://192.168.99.100:6379',
+  },
+};
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice(AppModule, microserviceOptions);
+  app.listen(() => {
+    logger.log('Microservice is listening ...');
+  });
+}
+bootstrap();
